d76f7b5ae660d5d257531ed20d89e749578e6e22
geugenio <geugnio12@gmail.com>
2023-12-15 09:21:13 -0300

    aula

---
+++ app/Controllers/Http/FavoritosController.ts
@@ -0,0 +1,44 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+const favoritos = [{id: 1, nome: 'IFRN', url: 'www.ifrn.edu.br', importante: false }]
+export default class FavoritosController {
+  public async index({}: HttpContextContract) {
+    return favoritos
+  }
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({ request, response }: HttpContextContract) {
+    const{nome,url,importante} = request.body()
+    const newFavorito = {id:favoritos.length+1,nome,url,importante}
+    favoritos.push(newFavorito)
+    return response.status(201).send(newFavorito)
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    //retornar o objeto caso exista, senao retornar objeto va
+    //função callback
+    let favoritoEncontrado = favoritos.find((favorito) => favorito.id == params.id)
+    if favoritoEncontrado == undefined
+      return response.status(404)
+    return favoritoEncontrado
+  }
+  public async update({ request, params, response }: HttpContextContract) {
+    const { nome, url, importante } = request.body()
+    let favoritoEncontrado = favoritos.find((favorito) => favorito.id == params.id)
+    if (!favoritoEncontrado) return response.status(404)
+    favoritoEncontrado.nome = nome
+    favoritoEncontrado.url = url
+    favoritoEncontrado.importante = importante
+
+    favoritos[params.id] = favoritoEncontrado
+    return response.status(200).send(favoritoEncontrado)
+  }
+
+  public async destroy({params, response}: HttpContextContract) {
+    let favoritoEncontrado = favoritos.find((favorito) => favorito.id == params.id)
+    if(!favoritoEncontrado)
+      return response.status(404)
+    favoritos.splice(favoritos.indexOf(favoritoEncontrado),1)
+    return response.status(204)
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -87,3 +87,4 @@ Route.put('/favoritos/:id', async ({response, params, request}) =>{
     return response.status(404)
   }
 })
+Route.resource('favoritos', 'favoritosController').apiOnly
